db1ef3ba32edf343109ea4b38437347e7b3a7fb2
Sierra Sutherland-Brown <sierrabardot@live.com.au>
2024-03-22 20:50:58 +1100

    Create function to add new location to db & save touser_interactions

--- src/config/dbClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const dbUrl = import.meta.env.VITE_APP_SUPABASE_URL;
-const dbKey = import.meta.env.VITE_APP_SUPABASE_KEY;
-const db = createClient(dbUrl, dbKey);
-
-export default db;
